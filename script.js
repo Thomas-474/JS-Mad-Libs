385907f2329af2a62abe99e9535d30b918569ee3
Thomas-474 <teg2004@icloud.com>
2021-08-26 10:41:23 -0400

    Other 2 Mad Libs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var adjective
 var place
 var number
-var animal
+var creature
 var verb
  
 document.getElementById('endResult').style.display = 'none';
@@ -10,10 +10,10 @@ function MadLib1(){
    adjective = document.getElementById('adjInput').value;
    place = document.getElementById('placeInput').value;
    number = document.getElementById('numbInput').value;
-   animal = document.getElementById('animalInput').value;
+   creature = document.getElementById('creatureInput').value;
    verb = document.getElementById('verbInput').value;
  
-   if (document.getElementById('adjInput').value == '' || document.getElementById('placeInput').value == '' || document.getElementById('numbInput').value == '' || document.getElementById('animalInput').value == '' || document.getElementById('verbInput').value == ''){
+   if (document.getElementById('adjInput').value == '' || document.getElementById('placeInput').value == '' || document.getElementById('numbInput').value == '' || document.getElementById('creatureInput').value == '' || document.getElementById('verbInput').value == ''){
        alert('You must fill out each text box!');
        return;
    }
@@ -21,17 +21,17 @@ function MadLib1(){
    document.getElementById('userInput').style.display = 'none';
    document.getElementById('endResult').style.display = 'block';
  
-   document.getElementById('result').innerHTML = `The party of ${adjective} adventurers were traveling to the next ${place}. On the way there they were attacked by ${number} ${animal}. The adventurers won the battle and proudly ${verb} the rest of the way to the city.`;
+   document.getElementById('result').innerHTML = `The party of ${adjective} adventurers were traveling to the next ${place}. On the way there they were attacked by ${number} ${creature}. The adventurers won the battle and proudly ${verb} the rest of the way to the city.`;
 }
  
 function MadLib2(){
    adjective = document.getElementById('adjInput').value;
    place = document.getElementById('placeInput').value;
    number = document.getElementById('numbInput').value;
-   animal = document.getElementById('animalInput').value;
+   creature = document.getElementById('creatureInput').value;
    verb = document.getElementById('verbInput').value;
  
-   if (document.getElementById('adjInput').value == '' || document.getElementById('placeInput').value == '' || document.getElementById('numbInput').value == '' || document.getElementById('animalInput').value == '' || document.getElementById('verbInput').value == ''){
+   if (document.getElementById('adjInput').value == '' || document.getElementById('placeInput').value == '' || document.getElementById('numbInput').value == '' || document.getElementById('creatureInput').value == '' || document.getElementById('verbInput').value == ''){
        alert('You must fill out each text box!');
        return;
    }
@@ -39,17 +39,17 @@ function MadLib2(){
    document.getElementById('userInput').style.display = 'none';
    document.getElementById('endResult').style.display = 'block';
  
-   document.getElementById('result').innerHTML = `22222222222222222222222222222222222222 The party of ${adjective} adventurers were traveling to the next ${place}. On the way there they were attacked by ${number} ${animal}. The adventurers won the battle and proudly ${verb} the rest of the way to the city.`;
+   document.getElementById('result').innerHTML = `22222222222222222222222222222222222222 The party of ${adjective} adventurers were traveling to the next ${place}. On the way there they were attacked by ${number} ${creature}. The adventurers won the battle and proudly ${verb} the rest of the way to the city.`;
 }
  
 function MadLib3(){
    adjective = document.getElementById('adjInput').value;
    place = document.getElementById('placeInput').value;
    number = document.getElementById('numbInput').value;
-   animal = document.getElementById('animalInput').value;
+   creature = document.getElementById('creatureInput').value;
    verb = document.getElementById('verbInput').value;
  
-   if (document.getElementById('adjInput').value == '' || document.getElementById('placeInput').value == '' || document.getElementById('numbInput').value == '' || document.getElementById('animalInput').value == '' || document.getElementById('verbInput').value == ''){
+   if (document.getElementById('adjInput').value == '' || document.getElementById('placeInput').value == '' || document.getElementById('numbInput').value == '' || document.getElementById('creatureInput').value == '' || document.getElementById('verbInput').value == ''){
        alert('You must fill out each text box!');
        return;
    }
@@ -57,7 +57,7 @@ function MadLib3(){
    document.getElementById('userInput').style.display = 'none';
    document.getElementById('endResult').style.display = 'block';
  
-   document.getElementById('result').innerHTML = `3333333333333333333333333333333333333333333 The party of ${adjective} adventurers were traveling to the next ${place}. On the way there they were attacked by ${number} ${animal}. The adventurers won the battle and proudly ${verb} the rest of the way to the city.`;
+   document.getElementById('result').innerHTML = `3333333333333333333333333333333333333333333 The party of ${adjective} adventurers were traveling to the next ${place}. On the way there they were attacked by ${number} ${creature}. The adventurers won the battle and proudly ${verb} the rest of the way to the city.`;
 }
  
 function MadLibRandom(){
